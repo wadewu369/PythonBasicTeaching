d30a344f4f1a422f9c144e6311466ff8af8bbc17
mark <ak790718@gmail.com>
2020-03-18 20:50:52 +0800

    更新 取得油價代碼 表格會更動

--- DataAnalysis/Pandas/Chapter_1/getOilData.py
+++ DataAnalysis/Pandas/Chapter_1/getOilData.py
@@ -8,8 +8,13 @@ date = datetime.now().strftime("%Y/%m/%d")
 
 
 def getTodayOilData():
-    readData = pd.read_html('https://www2.moeaboe.gov.tw/oil102/oil2017/A01/A0108/tablesprices.asp')[0]  # 取得網頁上的表格資訊
-    getOilNewDate = readData.loc[1][6][0:10]  # 取得第一列第六欄的字串前10個字元 也就是 日期
+    try:
+        readData = pd.read_html('https://www2.moeaboe.gov.tw/oil102/oil2017/A01/A0108/tablesprices.asp')[1]  # 取得網頁上的表格資訊
+        getOilNewDate = readData.loc[1][6][0:10]  # 取得第一列第六欄的字串前10個字元 也就是 日期
+    except:
+        readData = pd.read_html('https://www2.moeaboe.gov.tw/oil102/oil2017/A01/A0108/tablesprices.asp')[0]  # 取得網頁上的表格資訊
+        getOilNewDate = readData.loc[1][6][0:10]  # 取得第一列第六欄的字串前10個字元 也就是 日期
+
     print("官網提供歷史紀錄↓↓↓\n%s" % readData)
     print("\n取得最新日期↓↓↓\n%s" % getOilNewDate)
     if date == str(getOilNewDate):  # 判斷今天日期是否與最新日期相同，不相同則回傳False
